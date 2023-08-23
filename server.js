7d2dde09fd345ffaffa59e0849366796894fcef0
anotherinthefire <rongodfreyultra@gmail.com>
2023-08-23 08:26:02 +0800

    fixiefixie

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const { MongoClient } = require("mongodb")
 const PORT = process.env.PORT || 8000;
 
+app.use(cors())
 app.use(express.json({ extend: false }));
 
 const withDB = async (operations, res) => {
